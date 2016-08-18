c1f64f83953b14475f46e0ab3fb4cc4fc289270f
IlyaV <ilya@codeplay.ru>
2016-08-19 00:34:18 +0300

    WIP: minor fixes with toggle sliding

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
   $('.button').on('click', function() {
     $('.content').toggleClass('isOpen');
+    $('.title').toggle();
+  });
+
+  $('.sidebar ul li a').on('click', function(){
+  	var tab = $(this).text();
+    $('.content h1').text(tab);
+    $('.sidebar ul li a').removeClass('active');
+    $(this).addClass('active');
   });
 });
